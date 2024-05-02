a5f170840332482999fece8087b2b76e247676ef
PitchLost <thundergames221@gmail.com>
2024-05-02 12:16:29 +1200

    Adjust comments

--- main.js
+++ main.js
@@ -18,16 +18,16 @@ function generateNum() {
 
     // Define probability thresholds for each outcome within the range [min, max)
     const thresholds = [
-        { value: 195, outcome: 1, chance: '15' },   // Probability of getting 1: 15% (0% to 15%)
-        { value: 390, outcome: 2, chance: '15' },   // Probability of getting 2: 15% (cumulative 0-2)
-        { value: 585, outcome: 3, chance: '15' },   // Probability of getting 3: 15% (cumulative 0-3)
-        { value: 780, outcome: 4, chance: '15' },   // Probability of getting 4: 15% (cumulative 0-4)
-        { value: 975, outcome: 5, chance: '15' },   // Probability of getting 5: 15% (cumulative 0-5)
-        { value: 1105, outcome: 6, chance: '5' },  // Probability of getting 6: 5% (15% to 20%)
-        { value: 1227, outcome: 7, chance: '2' },  // Probability of getting 7: 2% (20% to 22%)
-        { value: 1260, outcome: 8, chance: '1' },  // Probability of getting 8: 1% (22% to 23%)
-        { value: 1295, outcome: 9, chance: '1' },  // Probability of getting 9: 1% (23% to 24%)
-        { value: 1300, outcome: 10, chance: '1' }  // Probability of getting 10: 1% (24% to 25%)
+        { value: 195, outcome: 1, chance: '15' },   // Probability of getting 1: 15% 
+        { value: 390, outcome: 2, chance: '15' },   // Probability of getting 2: 15% 
+        { value: 585, outcome: 3, chance: '15' },   // Probability of getting 3: 15% 
+        { value: 780, outcome: 4, chance: '15' },   // Probability of getting 4: 15%
+        { value: 975, outcome: 5, chance: '15' },   // Probability of getting 5: 15%
+        { value: 1105, outcome: 6, chance: '5' },  // Probability of getting 6: 5%
+        { value: 1227, outcome: 7, chance: '2' },  // Probability of getting 7: 2% 
+        { value: 1260, outcome: 8, chance: '1' },  // Probability of getting 8: 1% 
+        { value: 1295, outcome: 9, chance: '1' },  // Probability of getting 9: 1% 
+        { value: 1300, outcome: 10, chance: '1' }  // Probability of getting 10: 1% 
     ];
 
     // Determine the outcome based on the seed value
