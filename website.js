de82816fc522d5232c50775dfc9366040cfadc98
PitchLost <thundergames221@gmail.com>
2024-05-07 09:11:30 +1200

    Logo on bottom of page

--- website.js
+++ website.js
@@ -4,14 +4,13 @@ const app = Vue.createApp({
             links: [
                 { name: 'Random number generator', link: 'https://able-rail.cloudvent.net/' },
                 { name: 'Kosovo Sheets', link: 'https://docs.google.com/spreadsheets/d/1O-EO8SEVfB-6qoxzBIxsO82soKP8axk1tnoc1yE2EqE/edit#gid=1160569927' },
-                { name: '', link: '' },
-                { name: '', link: '' }
+                { name: 'Card Sheets', link: 'https://docs.google.com/spreadsheets/d/1DeCPlOKo2OK9BgFeN3G8Dv-vTwx7K7PCWnnUeGAxxdY/edit#gid=0' },
+                { name: 'Order Calculator', link: '' }
             ]
         };
     },
     methods: {
         navigateUser(link) {
-            // Implement navigation logic here
             window.open(link, '_blank'); // Opens link in a new tab
         }
     }
