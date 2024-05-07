6633fd3150b36623b28eae411888efa0dd771a96
PitchLost <thundergames221@gmail.com>
2024-05-07 14:34:17 +1200

    Isolated + Added link

--- website.js
+++ website.js
@@ -5,7 +5,7 @@ const app = Vue.createApp({
                 { name: 'Random number generator', link: 'https://able-rail.cloudvent.net/' },
                 { name: 'Kosovo Sheets', link: 'https://docs.google.com/spreadsheets/d/1O-EO8SEVfB-6qoxzBIxsO82soKP8axk1tnoc1yE2EqE/edit#gid=1160569927' },
                 { name: 'Card Sheets', link: 'https://docs.google.com/spreadsheets/d/1DeCPlOKo2OK9BgFeN3G8Dv-vTwx7K7PCWnnUeGAxxdY/edit#gid=0' },
-                { name: 'Order Calculator', link: '' }
+                { name: 'Order Calculator', link: 'https://pro-grasshopper.cloudvent.net/' }
             ]
         };
     },
