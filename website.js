29cfe1495aa1fa430aacc89306556836795dcdf9
PitchLost <thundergames221@gmail.com>
2024-05-07 14:47:43 +1200

    Fixed

---
+++ website.js
@@ -0,0 +1,19 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            links: [
+                { name: 'Random number generator', link: 'https://able-rail.cloudvent.net/' },
+                { name: 'Kosovo Sheets', link: 'https://docs.google.com/spreadsheets/d/1O-EO8SEVfB-6qoxzBIxsO82soKP8axk1tnoc1yE2EqE/edit#gid=1160569927' },
+                { name: 'Card Sheets', link: 'https://docs.google.com/spreadsheets/d/1DeCPlOKo2OK9BgFeN3G8Dv-vTwx7K7PCWnnUeGAxxdY/edit#gid=0' },
+                { name: 'Order Calculator', link: 'https://pro-grasshopper.cloudvent.net/' }
+            ]
+        };
+    },
+    methods: {
+        navigateUser(link) {
+            window.open(link, '_blank'); // Opens link in a new tab
+        }
+    }
+});
+
+app.mount('#content');
