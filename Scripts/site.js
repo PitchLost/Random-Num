f1bd0b3ff167f9a02d01c182e4a88397f532e2ac
PitchLost <thundergames221@gmail.com>
2024-05-09 13:10:32 +1200

    Start bug report

---
+++ Scripts/site.js
@@ -0,0 +1,7 @@
+// Variables: 
+const createSec = document.getElementById('create-hidden')
+const prevTickets = document.getElementById('tickets')
+// Functions: 
+function windowOpen() { 
+window.open('createticket.html')
+}
